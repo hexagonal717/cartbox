52accb2aad24b57438966eaa78aa190893512441
Calvin Paiva <calvin2paiva@gmail.com>
2024-05-23 19:52:06 +0530

    Add button for page navigation

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,5 +1,24 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+const MainContainer = styled.div`
+  display: flex;
+  position: fixed;
+  gap: 1rem;
+`;
+
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <MainContainer>
+      <div>Home</div>
+      <Link to={"/signup"}>
+        <button>Sign up Page</button>
+      </Link>
+      <Link to={"/login"}>
+        <button>Log In Page</button>
+      </Link>
+    </MainContainer>
+  );
 };
 
 export default Home;
