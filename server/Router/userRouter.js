011185480cc299791eebe249deb0694e130fafa4
Calvin Paiva <calvin2paiva@gmail.com>
2024-05-24 02:27:17 +0530

    add sign up validation logic

--- server/Router/userRouter.js
+++ server/Router/userRouter.js
@@ -2,7 +2,42 @@ const router = require("express").Router();
 const userInfo = require("../Model/userSchema");
 
 router.post("/signup", async (req, res) => {
-  console.log(req.body);
+    try {
+        console.log(req.body);
+        const {firstName, lastName, email, phone, password} = req.body;
+
+        if (!firstName || !lastName || !email || !phone || !password) {
+
+            console.log("All fields are required.")
+
+            return res.status(400).json({error: "All fields are required."});
+
+        }
+
+        const existingUser = await userInfo.findOne({email}, "_id", {lean: true});
+        if (existingUser) {
+            console.log("User already exists.")
+            return res.status(400).json({error: "User already exists."});
+
+        }
+
+        const newUser = new userInfo({
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            phone: req.body.phone,
+            password: req.body.password,
+        })
+        console.log(newUser)
+        await newUser.save();
+
+    } catch (err) {
+
+        console.error("Error during user signup:", err);
+        res.status(500).json({error: "Server error. Please try again later."});
+
+    }
+
 });
 
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -8,12 +8,12 @@ const userRouter = require("./Router/userRouter");
 
 app.use(cors());
 mongoose.connect(process.env.mongoUrl).then(() => {
-  console.log("Database connected.");
+    console.log("Database connected.");
 });
 
 app.use(express.json());
 app.use("/api/user", userRouter);
 
 app.listen(3000, () => {
-  console.log("Port connected.");
+    console.log(`Port connected.`);
 });
