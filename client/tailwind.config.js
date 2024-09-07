7804d2b29e5ba8290d1ef5dafca202ceea3bccd7
Calvin Paiva <calvinpaivaa@gmail.com>
2024-09-07 22:44:29 +0530

    feat: setup shadcn

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,29 +1,74 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   mode: 'jit',
-  darkMode: 'class',
+  darkMode: ['class', 'class'],
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        springgreen: {
-          50: '#e6fef0',
-          100: '#b3f5d2',
-          200: '#80ebb5',
-          300: '#4ae298',
-          400: '#1ddf7d',
-          500: '#00ff7f',
-          600: '#00e773',
-          700: '#00c66b',
-          800: '#00a763',
-          900: '#008c55',
-          950: '#005b39',
-        },
-      },
-      fontFamily: {
-        inter: ['Inter Variable, sans-serif'],
-      },
-    },
+  	extend: {
+  		colors: {
+  			springgreen: {
+  				'50': '#e6fef0',
+  				'100': '#b3f5d2',
+  				'200': '#80ebb5',
+  				'300': '#4ae298',
+  				'400': '#1ddf7d',
+  				'500': '#00ff7f',
+  				'600': '#00e773',
+  				'700': '#00c66b',
+  				'800': '#00a763',
+  				'900': '#008c55',
+  				'950': '#005b39'
+  			},
+  			background: 'hsl(var(--background))',
+  			foreground: 'hsl(var(--foreground))',
+  			card: {
+  				DEFAULT: 'hsl(var(--card))',
+  				foreground: 'hsl(var(--card-foreground))'
+  			},
+  			popover: {
+  				DEFAULT: 'hsl(var(--popover))',
+  				foreground: 'hsl(var(--popover-foreground))'
+  			},
+  			primary: {
+  				DEFAULT: 'hsl(var(--primary))',
+  				foreground: 'hsl(var(--primary-foreground))'
+  			},
+  			secondary: {
+  				DEFAULT: 'hsl(var(--secondary))',
+  				foreground: 'hsl(var(--secondary-foreground))'
+  			},
+  			muted: {
+  				DEFAULT: 'hsl(var(--muted))',
+  				foreground: 'hsl(var(--muted-foreground))'
+  			},
+  			accent: {
+  				DEFAULT: 'hsl(var(--accent))',
+  				foreground: 'hsl(var(--accent-foreground))'
+  			},
+  			destructive: {
+  				DEFAULT: 'hsl(var(--destructive))',
+  				foreground: 'hsl(var(--destructive-foreground))'
+  			},
+  			border: 'hsl(var(--border))',
+  			input: 'hsl(var(--input))',
+  			ring: 'hsl(var(--ring))',
+  			chart: {
+  				'1': 'hsl(var(--chart-1))',
+  				'2': 'hsl(var(--chart-2))',
+  				'3': 'hsl(var(--chart-3))',
+  				'4': 'hsl(var(--chart-4))',
+  				'5': 'hsl(var(--chart-5))'
+  			}
+  		},
+  		fontFamily: {
+  			inter: ['Inter Variable, sans-serif']
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,14 @@
 import {defineConfig} from 'vite'
 import * as million from "million/compiler";
 import react from '@vitejs/plugin-react'
-
+import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [million.vite(), react()],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
+    },
 })
